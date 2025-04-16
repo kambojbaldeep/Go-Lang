24ea111aa86ef2434de84fc2fe4dad69f9165d7f
Baldeep Singh <kambojbaldeep@gmail.com>
2025-04-16 18:44:35 +0530

    Concatenating two strings assignemnt

---
+++ L6-Concatenationg_strings_assignment.go
@@ -0,0 +1,33 @@
+/* Assignment
+Textio uses basic authentication to log users in.
+
+The code on the right has a type error. Change the type of password to a string (but use the same numeric value) so that it can be concatenated with the username variable.
+
+The code is: 
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "presidentSkroob"
+	var password int = 12345
+
+	// don't edit below this line
+	fmt.Println("Authorization: Basic", username+":"+password)
+}
+
+*/
+
+// The Solution is:
+
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "presidentSkroob"
+	var password string = "12345"
+
+	// don't edit below this line
+	fmt.Println("Authorization: Basic", username+":"+password)
+}
